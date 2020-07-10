b4dce3852191cf2394afafb71f31d6232a263e63
Christopher <cspeer@gmail.com>
2020-07-10 15:31:37 +0200

    too much

--- backup/src/gallery.js
+++
@@ -1,9 +0,0 @@
-import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
-import GalleryEditing from './editing.js';
-import GalleryUI from './ui.js';
-
-export default class Gallery extends Plugin {
-  static get requires() {
-    return [GalleryEditing, GalleryUI];
-  }
-}
